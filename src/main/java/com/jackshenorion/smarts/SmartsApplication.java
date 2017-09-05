5f13c45242cb4bdd30f876dd51838d854e4cda0c
Jack Shen <shenpinyi@gmail.com>
2017-09-05 21:30:29 +1000

    initial commit

---
+++ src/main/java/com/jackshenorion/smarts/SmartsApplication.java
@@ -0,0 +1,12 @@
+package com.jackshenorion.smarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/jackshenorion/smarts/util/mapper/Mapper.java
@@ -0,0 +1,14 @@
+package com.jackshenorion.smarts.util.mapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapper {
+
+    private Map<String, String> mapping = new HashMap<>();
+
+    // add
+    // delete
+    // getTarget
+
+}
